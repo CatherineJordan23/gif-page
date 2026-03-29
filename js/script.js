741fbd81cd0c6ff6365e4e1fc316deb2db1b3e44
CatherineJordan23 <leilani111305@gmail.com>
2026-03-28 21:06:50 -0400

    Fetch data from Giphy API and store image URLs

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 console.log("script.js loaded");
+
+let endpoint = "https://api.giphy.com/v1/gifs/search?api_key=HnUhD6u2Wm6pZ6fDLZD4Hoj22DFCtSnX&q=gif&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
+
+let images = [];
+
+fetch(endpoint)
+.then(function(response) {
+    return response.json();
+})
+
+.then(function(data) {
+    // data.data is an array of GIF objects
+    images = data.data.map(function(gif) {
+        return gif.images.original.url;
+    });
+    console.log(images);
+})
+.catch(function(error) {
+    console.log("Error:", error);
+});
